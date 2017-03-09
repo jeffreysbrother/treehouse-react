893c49568f1f8ffaf3582e00b034ff954a4bd6c4
jeffreysbrother <jeffreysbrother@gmail.com>
2017-03-08 18:07:46 -0800

    replaced a stateless functional component with a component class

--- app.jsx
+++ app.jsx
@@ -30,15 +30,29 @@ Header.propTypes = {
   title: React.PropTypes.string.isRequired,
 }
 
-function Counter(props) {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <div className="counter-score"> {props.score} </div>
-      <button className="counter-action increment"> + </button>
-    </div>
-  );
-}
+// component class (versus a stateless functional component)
+var Counter = React.createClass({
+  render: function() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <div className="counter-score"> {this.props.score} </div>
+        <button className="counter-action increment"> + </button>
+      </div>
+    );
+  }
+});
+
+// unused statless functional component EXAMPLE
+// function Counter(props) {
+//   return (
+//     <div className="counter">
+//       <button className="counter-action decrement"> - </button>
+//       <div className="counter-score"> {props.score} </div>
+//       <button className="counter-action increment"> + </button>
+//     </div>
+//   );
+// }
 
 Counter.propTypes = {
   score: React.PropTypes.number.isRequired,
