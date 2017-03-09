7e6127c84db347c6edd1ffb9a7ae69045c2d4118
jeffreysbrother <jeffreysbrother@gmail.com>
2017-03-08 17:07:07 -0800

    basic schtuff: static app

--- app.jsx
+++ app.jsx
@@ -3,9 +3,25 @@
 // components should start with a capital letter
 function Application() {
   return (
-    <div>
-      <h1>Hello from React!</h1>
-      <p>the heading above and this paragraph were rendered from the Application component.</p>
+    <div className="scoreboard">
+      <div className="header">
+        <h1>Scoreboard</h1>
+      </div>
+
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            James C.
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 31 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
