7ea614709f1ff98395d452512ac37b396805b13c
jeffreysbrother <jeffreysbrother@gmail.com>
2017-03-08 17:25:50 -0800

    decomposing

--- app.jsx
+++ app.jsx
@@ -1,26 +1,48 @@
 // ReactDOM.render(<h1>Hey Dude</h1>, document.getElementById('container'));
 
+function Header(props) {
+  return (
+    <div className="header">
+      <h1>{props.title}</h1>
+    </div>
+  );
+}
+
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired,
+}
+
+function Player(props) {
+  return (
+    <div className="player">
+      <div className="player-name">
+        {props.name}
+      </div>
+      <div className="player-score">
+        <div className="counter">
+          <button className="counter-action decrement"> - </button>
+          <div className="counter-score"> {props.score} </div>
+          <button className="counter-action increment"> + </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Player.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.number.isRequired,
+}
+
 // components should start with a capital letter
 function Application(props) {
   return (
     <div className="scoreboard">
-      <div className="header">
-        <h1>{props.title}</h1>
-      </div>
+      <Header title={props.title} />
 
       <div className="players">
-        <div className="player">
-          <div className="player-name">
-            James Cool
-          </div>
-          <div className="player-score">
-            <div className="counter">
-              <button className="counter-action decrement"> - </button>
-              <div className="counter-score"> 31 </div>
-              <button className="counter-action increment"> + </button>
-            </div>
-          </div>
-        </div>
+        <Player name="Jim Hoskins" score={31} />
+        <Player name="James Cool" score={300} />
       </div>
     </div>
   );
