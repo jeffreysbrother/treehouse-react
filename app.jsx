169cb0116d34ba5c96ace397b98cb10e925a0969
jeffreysbrother <jeffreysbrother@gmail.com>
2017-03-08 19:58:59 -0800

    add stopwatch component

--- app.jsx
+++ app.jsx
@@ -22,6 +22,21 @@ var nextId = 4;
 
 
 
+var Stopwatch = React.createClass({
+  render: function() {
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <div className="stopwatch-time">0</div>
+        <button>Start</button>
+        <button>Reset</button>
+      </div>
+    );
+  }
+});
+
+
+
 var AddPlayerForm = React.createClass({
   propTypes: {
     onAdd: React.PropTypes.func.isRequired,
@@ -88,6 +103,7 @@ function Header(props) {
     <div className="header">
       <Stats players={props.players}/>
       <h1>{props.title}</h1>
+      <Stopwatch />
     </div>
   );
 }
